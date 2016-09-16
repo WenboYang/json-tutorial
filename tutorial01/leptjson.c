1ac4a010308bd41a3dc4145b1fa8ad8420319bb3
WenboYang <wenbo.yangcn@gmail.com>
2016-09-16 08:51:02 +0800

    Add: implement parsing for true and false

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -15,7 +15,6 @@ static void lept_parse_whitespace(lept_context* c) {
     c->json = p;
 }
 
-/*#include <stdio.h>*/
 static int lept_parse_null(lept_context* c, lept_value* v) {
     EXPECT(c, 'n');
 
@@ -23,11 +22,47 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
         return LEPT_PARSE_INVALID_VALUE;
     c->json += 3;
     lept_parse_whitespace(c);
-    /*printf( "parsed to %c\n", *(c->json));*/
     if ( *( c->json) == '\0' )
     {
        v->type = LEPT_NULL;
-        return  LEPT_PARSE_OK;
+       return  LEPT_PARSE_OK;
+    }
+    else
+    {
+       return  LEPT_PARSE_ROOT_NOT_SINGULAR;	
+    }
+}
+
+
+static int lept_parse_true(lept_context* c, lept_value* v) {
+    EXPECT(c, 't');
+
+    if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 3;
+    lept_parse_whitespace(c);
+    if ( *( c->json) == '\0' )
+    {
+       v->type = LEPT_TRUE;
+       return  LEPT_PARSE_OK;
+    }
+    else
+    {
+       return  LEPT_PARSE_ROOT_NOT_SINGULAR;	
+    }
+}
+
+static int lept_parse_false(lept_context* c, lept_value* v) {
+    EXPECT(c, 'f');
+
+    if (c->json[0] != 'a' || c->json[1] != 'l' || c->json[2] != 's' || c->json[3] != 'e' )
+        return LEPT_PARSE_INVALID_VALUE;
+    c->json += 4;
+    lept_parse_whitespace(c);
+    if ( *( c->json) == '\0' )
+    {
+       v->type = LEPT_FALSE;
+       return  LEPT_PARSE_OK;
     }
     else
     {
@@ -38,6 +73,8 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
         case 'n':  return lept_parse_null(c, v);
+        case 'f':  return lept_parse_false(c, v);
+        case 't':  return lept_parse_true(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
         default:   return LEPT_PARSE_INVALID_VALUE;
     }
