1b0d07a94b78f1b548ce6abb7da0776d3c9feec4
WenboYang <wenbo.yangcn@gmail.com>
2016-09-22 23:27:51 +0800

    Add: test cases for boundary values

--- tutorial01/test.c
+++ tutorial01/test.c
@@ -108,6 +108,9 @@ static void test_parse_number() {
     TEST_NUMBER( 1.234E+10, "1.234E+10" );
     TEST_NUMBER( 1.234E-10, "1.234E-10" );
     TEST_NUMBER( 0.0, "1e-10000"); /* must underflow */
+    TEST_NUMBER( 2.2250738585072009e-308, "2.2250738585072009e-308"); /* max subnormal double */
+    TEST_NUMBER( 1.7976931348623157e308, "1.7976931348623157e308"); /* max double */
+    TEST_NUMBER( 5e-324, "5e-324"); /*min subnormal positive double*/
 }
 
 
